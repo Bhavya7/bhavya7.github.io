acfc015cafb8b7007112e916a8f9b3a1c49e9836
Bhavya7 <bhavya.vootla@gmail.com>
2017-05-08 01:01:10 -0400

    Evolent Health
    Production ready application for maintaining contact information.

---
+++ NewWebAPI/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewWebAPI.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Status")]
+        public Nullable<bool> Status { get; set; }
+    }
+}
